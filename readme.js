cc39694f5b9f31c9c47070236cdfced7294e6962
Antony Jones <aj@desirableobjects.co.uk>
2018-05-08 18:07:48 +0100

    readme + ci

---
+++ readme.js
@@ -0,0 +1,29 @@
+# parcel-plugin-prepack
+
+[![Travis CI](https://travis-ci.org/antony/parcel-plugin-prepack.svg?branch=master)](https://travis-ci.org/antony/parcel-plugin-prepack.svg?branch=master)
+
+A parcel plugin that enables facebook's [prepack](https://prepack.io/) for bundling.
+
+## Project status
+
+Early stage - as is prepack!
+
+## Installation
+
+```bash
+yarn add parcel-plugin-prepack -D
+```
+
+or
+
+```bash
+npm install parcel-plugin-prepack -D
+```
+
+## Configuration
+
+None yet. Just install and go!
+
+## License
+
+MIT License
